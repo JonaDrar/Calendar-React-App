10a39163222fdef99c01c3cba53e35a4dd7d11db
Jonathan Araya <j.arayarobles@gmail.com>
2023-01-13 01:03:08 -0300

    Form validations added

--- src/calendar/components/CalendarModal.jsx
+++ src/calendar/components/CalendarModal.jsx
@@ -1,7 +1,18 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
+import { addHours, differenceInSeconds } from "date-fns";
+
+import Swal from "sweetalert2";
+import "sweetalert2/dist/sweetalert2.min.css";
+
 import Modal from "react-modal/lib/components/Modal";
 
+import ReactDatePicker, { registerLocale } from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import es from "date-fns/locale/es";
+
 import "../modal.css";
+
+registerLocale("es", es);
 
 const customStyles = {
   content: {
@@ -17,11 +28,59 @@ const customStyles = {
 Modal.setAppElement("#root");
 
 export const CalendarModal = () => {
+    
   const [isOpen, setIsOpen] = useState(true);
+  const [formSubmitted, setFormSubmitted] = useState(false);
+
+  const [formValues, setFormValues] = useState({
+    title: "Jonathan",
+    notes: "Araya",
+    start: new Date(),
+    end: addHours(new Date(), 2),
+  });
+
+  const titleClass = useMemo(() => {
+    if ( !formSubmitted ) return '';
+
+    return ( formValues.title.length > 0 )
+        ? 'is-valid'
+        : 'is-invalid';
+
+  }, [formValues.title, formSubmitted])
+
+  const onInputChanged = ({ target }) => {
+    setFormValues({
+      ...formValues,
+      [target.name]: target.value,
+    });
+  };
+
+  const onDateChanged = (event, changing) => {
+    setFormValues({
+      ...formValues,
+      [changing]: event,
+    });
+  };
 
   const onCloseModal = () => {
     console.log("cerrando modal");
     setIsOpen(false);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setFormSubmitted(true);
+
+    const difference = differenceInSeconds(formValues.end, formValues.start);
+
+    if (isNaN(difference) || difference <= 0) {
+      Swal.fire("Fechas incorrectas", "Revisar las fechas ingresadas", "error");
+      return;
+    }
+
+    if (formValues.title.length <= 0) return;
+
+    console.log(formValues);
   };
 
   return (
@@ -35,15 +94,32 @@ export const CalendarModal = () => {
     >
       <h1> Nuevo evento </h1>
       <hr />
-      <form className="container">
+      <form className="container" onSubmit={onSubmit}>
         <div className="form-group mb-2">
           <label>Fecha y hora inicio</label>
-          <input className="form-control" placeholder="Fecha inicio" />
+          <ReactDatePicker
+            selected={formValues.start}
+            onChange={(event) => onDateChanged(event, "start")}
+            className="form-control"
+            dateFormat="Pp"
+            showTimeSelect
+            locale="es"
+            timeCaption="Hora"
+          />
         </div>
 
         <div className="form-group mb-2">
           <label>Fecha y hora fin</label>
-          <input className="form-control" placeholder="Fecha inicio" />
+          <ReactDatePicker
+            minDate={formValues.start}
+            selected={formValues.end}
+            onChange={(event) => onDateChanged(event, "end")}
+            className="form-control"
+            dateFormat="Pp"
+            showTimeSelect
+            locale="es"
+            timeCaption="Hora"
+          />
         </div>
 
         <hr />
@@ -51,10 +127,12 @@ export const CalendarModal = () => {
           <label>Titulo y notas</label>
           <input
             type="text"
-            className="form-control"
+            className={`form-control ${titleClass}`}
             placeholder="Título del evento"
             name="title"
             autoComplete="off"
+            value={formValues.title}
+            onChange={onInputChanged}
           />
           <small id="emailHelp" className="form-text text-muted">
             Una descripción corta
@@ -64,10 +142,12 @@ export const CalendarModal = () => {
         <div className="form-group mb-2">
           <textarea
             type="text"
-            className="form-control"
+            className={`form-control ${titleClass}`}
             placeholder="Notas"
             rows="5"
             name="notes"
+            value={formValues.notes}
+            onChange={onInputChanged}
           ></textarea>
           <small id="emailHelp" className="form-text text-muted">
             Información adicional
